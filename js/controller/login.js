51346b139cdcfa7f0d0f1a7783ec3bb0810bff50
joe1i <146038157+joe1i@users.noreply.github.com>
2024-04-15 16:44:17 +0300

    Update login.js

--- js/controller/login.js
+++ js/controller/login.js
@@ -17,7 +17,7 @@ export default class Login_Controller {
         try {
             this.model.setData(email, password, remember);
             this.model.validate();
-          
+            windows.location = '/index.html'
             new Message_View().showSuccessMessage('Вхід виконано успішно!');
         } catch (error) {
             new Message_View().showErrorMessage(error.message);
